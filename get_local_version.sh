3f72166d66058cf4d4b15cf3044fbb93a877572c
Vishesh Handa <me@vhanda.in>
2020-03-23 14:10:34 +0100

    Update get local version script
    Makes it a bit easier

--- get_local_version.sh
+++ get_local_version.sh
@@ -8,7 +8,6 @@ echo
 FLUTTER_VERSION=`flutter --version | head -n 1 | awk '{ print $2 }'`
 URL="https://storage.googleapis.com/flutter_infra/releases/stable/linux/flutter_linux_v${FLUTTER_VERSION}-stable.tar.xz"
 
-set -x
 curl --head $URL
 
-
+echo "ENV FLUTTER_URL \"$URL\""
